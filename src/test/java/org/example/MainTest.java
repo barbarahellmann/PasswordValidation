91ca955cb4e64edc84d1720bd02efe0ff32cacd8
barbarahellmann <babshellmann@web.de>
2024-03-12 14:38:42 +0100

    Test for password length implemented

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
+
+    @Test
+    void passwordLengthIsMin8_shouldReturnTrue_whenLengthBigger8() {
+        //GIVEN
+        String password = "abcdefghi";
+        //WHEN
+        boolean actual = Main.passwordLengthIsMin8(password);
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void passwordLengthIsMin8_shouldReturnFalse_whenLengthSmaller78() {
+        //GIVEN
+        String password = "abc";
+        //WHEN
+        boolean actual = Main.passwordLengthIsMin8(password);
+        //THEN
+        Assertions.assertFalse(actual);
+    }
 }
